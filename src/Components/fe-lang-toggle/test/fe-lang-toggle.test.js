231ba7c9a1dcae686ca81df10fa9cb9f056a37dd
arestate <argoestate52@gmail.com>
2021-02-26 19:29:22 +0530

    Resolved Conflict

--- src/Components/fe-lang-toggle/test/fe-lang-toggle.test.js
+++ src/Components/fe-lang-toggle/test/fe-lang-toggle.test.js
@@ -8,13 +8,21 @@ describe('FeLangToggle', () => {
     element = await fixture(html`<fe-lang-toggle></fe-lang-toggle>`);
   });
 
-  it('renders a button Label', () => {
+  it('renders a first button Label', () => {
+    const button = element.shadowRoot.querySelector('#en-GB');
+    expect(button).to.exist;
+  });
+
+  it('renders a first button Label', () => {
+    const button2 = element.shadowRoot.querySelector('#nl-NL');
+    expect(button2).to.exist;
+  });
+
+  it('Checks the content of button', () => {
     const button = element.shadowRoot.querySelector('#en-GB');
     const button2 = element.shadowRoot.querySelector('#nl-NL');
-    expect(button).to.exist;
-    expect(button2).to.exist;
-    expect(button.textContent).to.equal('EN');
     expect(button2.textContent).to.equal('NL');
+    expect(button.textContent).to.equal('EN');
   });
 
   it('passes the a11y audit', async () => {
