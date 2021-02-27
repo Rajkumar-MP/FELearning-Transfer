86d4291f90440858c9a605cb25993a24c34bbfcd
Rajkumar.MP <mprajcs@gmail.com>
2021-02-27 15:54:24 +0530

    test: testcases updated

--- src/Components/fe-footer/test/fe-footer.test.js
+++ src/Components/fe-footer/test/fe-footer.test.js
@@ -1,4 +1,4 @@
-import { html, fixture, expect, assert } from '@open-wc/testing';
+import { html, fixture, expect, aTimeout, oneEvent } from '@open-wc/testing';
 
 import '../fe-footer.js';
 
@@ -8,18 +8,46 @@ describe('FeFooter', () => {
     element = await fixture(html`<fe-footer></fe-footer>`);
   });
 
-  it('renders a footer', () => {
+  xit('renders a footer', () => {
     const footer = element.shadowRoot.querySelector('footer');
 
     expect(footer).to.have.class('container');
-  
-   
   });
 
-  it('renders a class', () => {
+  xit('renders a class', () => {
     const div = element.shadowRoot.querySelector('div');
 
-    expect(div).to.have.id('button');  
+    expect(div).to.have.id('button');
+  });
+
+  it('should display both buttons when both params are passed', async () => {
+    element.primary = 'Next';
+    element.secondary = 'Back';
+    await aTimeout(10);
+    const buttonCount = element.shadowRoot.querySelectorAll('lion-button')
+      .length;
+    const [
+      primaryBtnElement,
+      secondaryBtnElement,
+    ] = element.shadowRoot.querySelectorAll('lion-button');
+    expect(buttonCount).to.be.equal(2);
+    expect(primaryBtnElement.textContent).to.be.equal('Next');
+    expect(secondaryBtnElement.textContent).to.be.equal('Back');
+  });
+
+  it('should display primary button when i pass primary parameter alone', () => {});
+  it('should display secondary button when i pass secondary parameter alone', () => {});
+  it('nothing to display when no params are passed in the component', () => {});
+  it('should trigger proper event when the specific button is clicked', async () => {
+    element.primary = 'Next';
+    element.secondary = 'Back';
+    await aTimeout(10);
+    const [primaryBtnElement] = element.shadowRoot.querySelectorAll(
+      'lion-button'
+    );
+    setTimeout(() => primaryBtnElement.click());
+    const { type } = await oneEvent(element, 'primary-btn-click');
+    expect(type).to.be.equal('primary-btn-click');
   });
 
   it('passes the a11y audit', async () => {
