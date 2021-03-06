e5b061627ccedc7bd7f2ee8a6f3f2037dbb3c97f
alekhya66 <79098742+alekhya66@users.noreply.github.com>
2021-03-06 18:17:40 +0530

    Feat/otp component (#22)
    * otp compo-added input field and stories
    
    * otp component-test cases added

---
+++ src/Components/fe-otp/test/fe-otp.test.js
@@ -0,0 +1,23 @@
+import { html, fixture, expect, oneEvent } from '@open-wc/testing';
+
+import '../fe-otp.js';
+
+describe('FeOtp', () => {
+  let element;
+  beforeEach(async () => {
+    element = await fixture(html`<fe-otp></fe-otp>`);
+  });
+
+  it('the input validation event should be triggered on clicking submit', async () => {
+    const otpcode = element.shadowRoot.querySelector('#otp-code');
+    otpcode.modelValue = 'otp-code';
+    const form = element.shadowRoot.querySelector('lion-form');
+    setTimeout(() => form.submit());
+    const { detail } = await oneEvent(element, 'input-validation');
+    expect(detail, { otpcode: 'otpcode' }).to.exist;
+  });
+
+  it('passes the a11y audit', async () => {
+    await expect(element).shadowDom.to.be.accessible();
+  });
+});
