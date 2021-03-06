91eef147a83f133a8dbd5971dba68b43e20b9b39
dalekhya66@gmail.com <dalekhya66@gmail.com>
2021-03-06 09:29:03 +0530

    otp component-test cases added

--- src/Components/fe-otp/test/fe-otp.test.js
+++ src/Components/fe-otp/test/fe-otp.test.js
@@ -1,4 +1,4 @@
-import { html, fixture, expect } from '@open-wc/testing';
+import { html, fixture, expect, oneEvent } from '@open-wc/testing';
 
 import '../fe-otp.js';
 
@@ -6,6 +6,15 @@ describe('FeOtp', () => {
   let element;
   beforeEach(async () => {
     element = await fixture(html`<fe-otp></fe-otp>`);
+  });
+
+  it('the input validation event should be triggered on clicking submit', async () => {
+    const otpcode = element.shadowRoot.querySelector('#otp-code');
+    otpcode.modelValue = 'otp-code';
+    const form = element.shadowRoot.querySelector('lion-form');
+    setTimeout(() => form.submit());
+    const { detail } = await oneEvent(element, 'input-validation');
+    expect(detail, { otpcode: 'otpcode' }).to.exist;
   });
 
   it('passes the a11y audit', async () => {
