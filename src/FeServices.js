f87fe659c34a4a10ee47d64a53821b0074d8819f
Rajkumar.MP <mprajcs@gmail.com>
2021-03-09 17:47:03 +0530

    Services added (#27)
    * Services added
    
    * Added json files
    
    * fix: prettier
    
    Co-authored-by: unknown <nayanaprabhakar14@gmail.com>

---
+++ src/FeServices.js
@@ -0,0 +1,30 @@
+import { ajax } from '@lion/ajax';
+
+const baseUrl = 'https://felearning-api.herokuapp.com';
+const getRequest = ({ url }) => {
+  const requestURL = `${baseUrl}${url}`;
+  return ajax
+    .request(requestURL)
+    .then(response => response.json())
+    .then(result => result)
+    .catch(error => {
+      console.log(error);
+    });
+};
+
+const postRequest = ({ url, data }) => {
+  const requestURL = `${baseUrl}${url}`;
+  return ajax
+    .requestJson(requestURL, {
+      method: 'POST',
+      body: data,
+    })
+    .then(result => result.body)
+    .catch(error => {
+      console.log(error);
+    });
+};
+export const FeServices = {
+  getRequest,
+  postRequest,
+};
